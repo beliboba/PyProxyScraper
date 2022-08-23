1b05048b20e4b392ba2d324442c0c2206598e9fe
beliboba <belibobka@gmail.com>
2022-08-23 16:12:08 +0300

    Add proxylist

--- scraper.py
+++ scraper.py
@@ -1,7 +1,10 @@
+import os
 import sys
 
 import aiofiles
 import asyncio
+
+import aiohttp
 from rich import print
 from rich.panel import Panel
 
@@ -24,7 +27,7 @@ async def hmn_scrape(output: str, ptype: str) -> None:
 	pagecount = int(pages.find_element(By.TAG_NAME, 'a').text)
 	scraped = 0
 	types = {"http": "h", "https": "s", "socks4": "4", "socks5": "5"}
-	for i in range(1, pagecount):
+	for i in range(pagecount):
 		print(Panel.fit(f"[blue bold]Страница: {i}[/]"))
 		driver.get(f'https://hidemy.name/ru/proxy-list/?type={types[ptype]}&start={i*64}#list')
 		rows = driver.find_element(By.XPATH, "/html/body/div[1]/div[4]/div/div[4]/table/tbody")
@@ -35,8 +38,8 @@ async def hmn_scrape(output: str, ptype: str) -> None:
 				if f"{ip}:{port}" not in await f.readlines():
 					await f.write(f"{ip}:{port}\n")
 			scraped += 1
-			print(Panel.fit(f"[green bold]{scraped}[/]"))
-	driver.quit()
+			os.system("clear | cls")
+			print(Panel.fit(f"[blue bold]HideMyName[/]\n[blue]Доступно прокси: {pagecount * 64}[/]\n[green]Scraped: {scraped}[/]"))
 
 
 async def fplist_scrape(output: str, ptype: str) -> None:
@@ -50,19 +53,39 @@ async def fplist_scrape(output: str, ptype: str) -> None:
 		print(Panel.fit(f"[red]Неподдерживаемый тип прокси. Поддерживаемые типы: http, https[/]"))
 		sys.exit(1)
 	driver.get('https://free-proxy-list.net/')
-	rows = driver.find_element(By.XPATH, "//*[@id='list']/div/div[2]/div/table/tbody")
-	for row in rows.find_elements(By.TAG_NAME, 'tr'):
+	rows = driver.find_element(By.XPATH, "//*[@id='list']/div/div[2]/div/table/tbody").find_elements(By.TAG_NAME, 'tr')
+	for row in rows:
 		ip = row.find_element(By.CSS_SELECTOR, "tr > td:nth-child(1)").get_attribute("innerHTML")
 		port = row.find_element(By.CSS_SELECTOR, "tr > td:nth-child(2)").get_attribute("innerHTML")
+		supports_type = row.find_element(By.CSS_SELECTOR, "tr > td:nth-child(6)").get_attribute("innerHTML")
 		async with aiofiles.open(output, 'a+') as f:
-			if f"{ip}:{port}" not in await f.readlines():
+			if f"{ip}:{port}" not in await f.readlines() and (types[ptype] == supports_type):
 				await f.write(f"{ip}:{port}\n")
 		scraped += 1
-		print(Panel.fit(f"[green bold]{scraped}[/]"))
+		os.system("clear | cls")
+		print(Panel.fit(f"[blue bold]FreeProxyList[/]\n[blue]Доступно прокси: {len(rows)}[/]\n[green]Scraped: {scraped}[/]"))
 
 
-def menu():
-	print(Panel.fit(f"[blue bold]Выберите тип прокси для скрапинга:[/]\n1. http\n2. https\n3. socks4\n socks5 \n [blue bold]Введите цифру[/]"))
+async def geonode_scraper(output: str, ptype: str):
+	async with aiohttp.ClientSession() as session:
+		async with session.get(f'https://proxylist.geonode.com/api/proxy-list?limit=0&page=1&sort_by=lastChecked&sort_type=desc&protocols={ptype}') as response:
+			print(Panel.fit(f"[green]Доступно {response.json['total']} прокси[/]"))
+			proxies = response.json['data']
+			scraped = 0
+			for proxy in proxies:
+				ip = proxy['ip']
+				port = proxy['port']
+				async with aiofiles.open(output, 'a+') as f:
+					if f"{ip}:{port}" not in await f.readlines():
+						await f.write(f"{ip}:{port}\n")
+				scraped += 1
+				print(Panel.fit(f"[green bold]{scraped}[/]"))
+				os.system("clear | cls")
+				print(Panel.fit(f"[blue bold]Geonode[/]\n[blue]Доступно прокси: {len(proxies)}[/]\n[green]Scraped: {scraped}[/]"))
+
+
+async def menu():
+	print(Panel.fit(f"[blue bold]Выберите тип прокси для скрапинга:[/]\n1. http\n2. https\n3. socks4\n4. socks5 \n [blue bold]Введите цифру[/]"))
 	types = {
 		1: "http",
 		2: "https",
@@ -74,15 +97,19 @@ def menu():
 	output = input()
 	hmn_types = ["http", "https", "socks4", "socks5"]
 	fplist_types = ["http", "https"]
-	if ptype in hmn_types and fplist_types:
-		print(Panel.fit(f"[green]Скраплю [bold]FreeProxyList[/] и [bold]HideMyName[/]"))
-		fplist_scrape(output, ptype)
-	elif ptype in hmn_types and not fplist_types:
-		print(Panel.fit(f"[green]Скраплю [bold]HideMyName[/][/]"))
-		hmn_scrape(output, ptype)
+	geonode_types = ["http", "https", "socks4", "socks5"]
+	if ptype in hmn_types and fplist_types and geonode_types:
+		print(Panel.fit(f"[green]Скраплю [bold]FreeProxyList[/], [bold]Geonode[/] и [bold]HideMyName[/]"))
+		await fplist_scrape(output, ptype)
+		await hmn_scrape(output, ptype)
+		await geonode_scraper(output, ptype)
+		driver.close()
+	elif ptype in fplist_types and not hmn_types and geonode_types:
+		print(Panel.fit(f"[green]Скраплю [bold]FreeProxyList[/][/]"))
+		await hmn_scrape(output, ptype)
 	else:
 		print(Panel.fit(f"[red]Ошибка произошла какая-то[/]"))
 
 
 if __name__ == "__main__":
-	menu()
+	asyncio.run(menu())
