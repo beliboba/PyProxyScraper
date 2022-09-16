9b14802b3e62e8a25e8a90a6a524d418f3cc89ff
beliboba <belibobka@gmail.com>
2022-09-16 21:53:49 +0300

    bebriki

--- main.py
+++ main.py
@@ -6,17 +6,6 @@ from rich import print
 from rich.panel import Panel
 
 
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
-options = Options()
-options.headless = True
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36")
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-
 async def menu():
 	ptypes = ['http', 'https', 'socks4', 'socks5']
 	for ptype in ptypes:
