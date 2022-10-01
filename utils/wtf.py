860cac3a43b94ff77a6c88837d8d47606c1a66c1
beliboba <belibobka@gmail.com>
2022-10-01 20:18:38 +0300

    docstring

--- utils/wtf.py
+++ utils/wtf.py
@@ -1,11 +1,8 @@
 import aiofiles
 
-"""
-Will write desired proxy to output file
-"""
-
 
 async def wtf(output: str, text: str):
+	"""Write text to file"""
 	async with aiofiles.open(output, 'a+') as f:
 		if text not in await f.readlines():
 			await f.write(text + "\n")
