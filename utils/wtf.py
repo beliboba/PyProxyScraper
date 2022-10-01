0005e94ffdc0c7e77ad7c9cc4612ee14d55f7308
beliboba <belibobka@gmail.com>
2022-10-01 20:19:32 +0300

    Fix duplicated proxies

--- utils/wtf.py
+++ utils/wtf.py
@@ -3,6 +3,8 @@ import aiofiles
 
 async def wtf(output: str, text: str):
 	"""Write text to file"""
+	async with aiofiles.open(output, 'r') as f:
+		contents = await f.readlines()
 	async with aiofiles.open(output, 'a+') as f:
-		if text not in await f.readlines():
+		if text + '\n' not in contents:
 			await f.write(text + "\n")
